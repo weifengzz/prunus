dd50bfe4500e1581161c10121cdf719a86344a57
weifengzz <weifengzz@outlook.com>
2019-01-16 17:45:26 +0800

    文字卡片

--- src/modules/home/component/cards/text_card.js
+++ src/modules/home/component/cards/text_card.js
@@ -15,7 +15,9 @@ import {
 } from 'react-native'
 import {
   TouchableOpacity,
-  fullScreen
+  fullScreen,
+  Icon,
+  SpliteLine
 } from '../../../../components'
 import { withNavigation } from 'react-navigation'
 
@@ -30,13 +32,64 @@ const IS_IOS = Platform.OS === 'ios'
  */
 class TextCard extends Component {
   _renderContent () {
-    return this._renderText()
+    return (
+      <View style={styles.contentView}>
+        {this._renderHeaderView()}
+        {this._renderText()}
+        {this._renderFooterView()}
+      </View>
+    )
   }
+
+  /**
+   * 渲染
+   */
+  _renderHeaderView () {
+    return (
+      <View style={styles.headerView}>
+        <View style={{ width: 30 }} />
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text style={styles.titleText}>开心一笑</Text>
+        </View>
+        <View style={{ width: 30 }}>
+          <Icon size={20} color={'white'} name={'sharealt'} type={'ant_design'} />
+        </View>
+      </View>
+    )
+  }
+
+  /**
+   * 文字
+   */
   _renderText () {
     const { text } = this.props
     return (
-      <View style={{ flex: 1, padding: 20 }}>
-        <Text style={{ fontSize: 20, color: 'white' }}>{text}</Text>
+      <View style={{ flex: 1, paddingHorizontal: 20, alignItems: 'center', justifyContent: 'center' }}>
+        <Text numberOfLines={10} style={{ fontSize: 18, color: 'white', lineHeight: 30 }}>{text}</Text>
+      </View>
+    )
+  }
+
+  _renderFooterView () {
+    return (
+      <View>
+        <SpliteLine color={'white'} />
+        <View style={styles.footerView}>
+          <View style={[styles.footerItemView, { flex: 2 }]}>
+            <View style={styles.itemLeftView}>
+              <Icon size={18} color={'#c3dcdd'} name={'smileo'} type={'ant_design'} />
+              <Text style={styles.commonSmallText}>100</Text>
+              <View style={{ width: 20 }} />
+              <Icon size={20} color={'#c3dcdd'} name={'eyeo'} type={'ant_design'} />
+              <Text style={styles.commonSmallText}>3.5k</Text>
+            </View>
+            <Icon size={18} color={'red'} name={'heart'} type={'ant_design'} />
+            <Text style={styles.commonSmallText}>2.2k</Text>
+            <View style={{ width: 20 }} />
+            <Icon size={18} color={'#c3dcdd'} name={'message1'} type={'ant_design'} />
+            <Text style={styles.commonSmallText}>80</Text>
+          </View>
+        </View>
       </View>
     )
   }
@@ -73,6 +126,10 @@ const styles = StyleSheet.create({
     width: CARD_WIDTH,
     borderRadius: 10
   },
+  contentView: {
+    height: '100%',
+    width: '100%'
+  },
   shadowStyle: {
     shadowColor: '#000',
     shadowOffset: {
@@ -81,6 +138,40 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.7,
     shadowRadius: 10
+  },
+  headerView: {
+    height: 80,
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    flexDirection: 'row'
+  },
+  footerView: {
+    height: 80,
+    width: CARD_WIDTH,
+    flexDirection: 'row'
+  },
+  footerItemView: {
+    paddingHorizontal: 10,
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  itemLeftView: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 5
+  },
+  commonSmallText: {
+    fontSize: 12,
+    color: '#ccd3dd',
+    marginLeft: 10,
+    alignSelf: 'center'
+  },
+  titleText: {
+    fontSize: 23,
+    color: 'white',
+    fontWeight: 'bold'
   }
 })
 
